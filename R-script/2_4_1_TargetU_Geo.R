3e1b804b6b325f3ccb26c202b3c5be4a583811d7
Skjaerven <ens@nve.no>
2016-11-08 12:24:50 +0100

    Beregner mønster RVK hsjordandel
    Også endret rekkefølge på id-henting i 1_4

---
+++ R-script/2_4_1_TargetU_Geo.R
@@ -0,0 +1,23 @@
+## Beregner Mønsterselskapets rammevilkår
+
+#Lager ny dataframe av kun normkostandeler for D-nett
+
+d_normkostandeler = dplyr::select(d_vekter.faktisk, contains("d_normk"))
+#Velger observerte RVK for referentene.
+##OBS! Her må vi finne en mer dynamisk måte å velge referentene på
+
+#Lager dataframe med kun observasjoner for D-nett
+d_ref.data = dplyr::filter(d_tilDEA, d_tilDEA$id %in% d_ref)
+
+# Trekker ut referentenes verdier for jordkabel
+d_ref.dm.hsjordand = d_ref.data$dr_hsjordand
+#transponerer matrisen med normkostandeler
+d_normkost.temp = t(d_normkostandeler)
+#Regner produktsummer pr kolonne, hver kolonne representerer da hver selskap
+dm_dr_hsjordand = colSums(d_ref.dm.hsjordand*d_normkost.temp)
+#legger verdiene i egen frame før de kobles på d_tilDEA
+df.dm_dr_hsjordand = data.frame(dm_dr_hsjordand)
+d_tilDEA = cbind.data.frame(d_tilDEA, df.dm_dr_hsjordand)
+
+rm(df.dm_dr_hsjordand, dm_dr_hsjordand)
+
