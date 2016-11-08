f01f5abfaa4fdfcf7fdd69ebc58db94d4762f0a3
Skjaerven <ens@nve.no>
2016-11-08 12:49:21 +0100

    Beregner mønster rvk s4

--- R-script/2_4_1_TargetU_Geo.R
+++ R-script/2_4_1_TargetU_Geo.R
@@ -13,7 +13,7 @@ d_ref.data = dplyr::filter(d_tilDEA, d_tilDEA$id %in% d_ref)
 d_ref.dm.hsjordand = d_ref.data$dr_hsjordand
 #transponerer matrisen med normkostandeler
 d_normkost.temp = t(d_normkostandeler)
-#Regner produktsummer pr kolonne, hver kolonne representerer da hver selskap
+#Regner produktsummer pr kolonne, hver kolonne representerer da hvert selskap
 dm_dr_hsjordand = colSums(d_ref.dm.hsjordand*d_normkost.temp)
 #legger verdiene i egen frame før de kobles på d_tilDEA
 df.dm_dr_hsjordand = data.frame(dm_dr_hsjordand)
@@ -21,3 +21,12 @@ d_tilDEA = cbind.data.frame(d_tilDEA, df.dm_dr_hsjordand)
 
 rm(df.dm_dr_hsjordand, dm_dr_hsjordand)
 
+#trekker ut referentes verdier for barskog
+d_ref.dm.s4 = d_ref.data$dr_s4
+#Regner produktsummer pr kolonne, hver kolonne representerer da hvert selskap
+dm_dr_s4 = colSums(d_ref.dm.s4*d_normkost.temp)
+df.dm_dr_s4 = data.frame(dm_dr_s4)
+#legger verdiene i egen frame før de kobles på d_tilDEA
+d_tilDEA = cbind.data.frame(d_tilDEA, df.dm_dr_s4)
+
+rm(df.dm_dr_s4, dm_dr_s4)
