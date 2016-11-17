69311fc01a14b81e34140ef2de332dc2f1cdb3b8
mohh <mohh@nve.no>
2016-11-17 10:45:39 +0100

    Kommer stadig videre på trinn 3. Må legge inn KPIer.

--- R-script/3_5 Kalibrering_D3.R
+++ R-script/3_5 Kalibrering_D3.R
@@ -12,60 +12,37 @@
 # 9. Beregner så kalibrert kostnadsnorm
 # 10. beregner kalibrert DEA-resultat ved å dele 7. på 3. 
 
-# ad 1: 
-#dat$d_selskap_tilkal_dummy <- 0
-#for (i in which(dat$aar == faktisk.aar & dat$d_tilDEA == 1)){
-#        dat[i,"Hvis 1, skal kalibreres i Dnett"]  = 1 
-#}
-#
-#dat$kal_selskap_tilkal_dummy <- 0
-#for (i in which(dat$d_selskap_tilkal_dummy == 1)){
-#        dat[i,"Hvis 1, selskap inngår i kalibreringsgrunnlag"] = 1
-#}
-# ad 2:
 
-# ad 3:
-d_tilDEA$d_kostnadsgrlag <- 0
-for (i in which(dat$aar == faktisk.aar)){
-        ((dat$d_DV*dat$kpia) + (dat$d_akg*nve.rente.t) + dat$d_avs + 
-        (dat$d_kile*dat$kpi) + (dat$d_nettap*kraftpris) - 
-        (dat$d_grs.cost*dat$kpi))   
-}
+# ad 3: trenger kanskje ikke gange med kpi siden det er 2014 tall?
+d_tilDEA$d_kostnadsgrlag <- ((d_tilDEA$d_DV*d_tilDEA$kpia) + (d_tilDEA$d_akg*nve.rente.t) + 
+                              d_tilDEA$d_avs + (d_tilDEA$d_kile*d_tilDEA$kpi) + 
+                             (d_tilDEA$d_nettap*kraftpris) - (d_tilDEA$d_grs.cost*d_tilDEA$kpi))   
      
 # ad 4:
 #dat$kostnadsnorm_tilkal <- (dat$d_deares_til_kal * dat$d_kostnadsgrlag)
-dat$kostnadsnorm_tilkal.snitt <- (d_tilDEA$d_sf_eff * dat$d_kostnadsgrlag)
-#dat$kostnadsnorm_tilkal.faktisk <- (d_tilDEA$d_f_sf_eff * dat$d_kostnadsgrlag)
+d_tilDEA$kostnadsnorm_tilkal.snitt <- (d_tilDEA$d_sf_eff * d_tilDEA$d_kostnadsgrlag)
+#d_tilDEA$kostnadsnorm_tilkal.faktisk <- (d_tilDEA$d_f_sf_eff*dat$d_kostnadsgrlag)
 
 # ad 5:
-summary(d_tilDEA$kostnadsgrlag)
-d_kostnadsgrlag_sum <-
+d_kostnadsgrlag_sum <- sum(d_tilDEA$d_kostnadsgrlag)
 
 # ad 6:
-summary(d_tilDEA$kostnadsnorm_tilkal.snitt)
-summary(d_tilDEA$kostnadsnorm_tilkal.faktisk)
-d_kostnadsnorm_tilkal.snitt <-
-d_kostnadsnorm_tilkal.faktisk <-
+d_kostnadsnorm_tilkal.snitt <- sum(d_tilDEA$kostnadsnorm_tilkal.snitt)
+d_kostnadsnorm_tilkal.faktisk <- sum(d_tilDEA$kostnadsnorm_tilkal.faktisk)
 
 # ad 7:
-dat$d_akg_kal <- dat$d_akg # Avkastningsgrunnlag Dnett
+d_tilDEA$d_akg_kal <- d_tilDEA$d_akg # Avkastningsgrunnlag Dnett
 
 # ad 8:
-summary(d_tilDEA$d_akg_kal)
-d_akg_kal_sum <-
+d_akg_kal_sum <- sum(d_tilDEA$d_akg_kal)
 
 # ad 9:
-dat$d_kostnadsnorm_kalAKG <- 0
-for (i in which(dat$d_selskap_tilkal_dummy == 1)){
-        dat$d_kostnadsnorm_tilkal + ((dat$d_kostnadsgrlag_sum - dat$d_kostnadsnorm_sum)* 
-        (dat$d_akg_kal/dat$d_akg_kal_sum))
-}
+d_tilDEA$d_kostnadsnorm_kalAKG <- (d_tilDEA$d_kostnadsnorm_tilkal + ((d_tilDEA$d_kostnadsgrlag_sum - d_tilDEA$d_kostnadsnorm_sum) * 
+        (d_tilDEA$d_akg_kal/d_tilDEA$d_akg_kal_sum))) 
 
 # ad 10:
-dat$d_escore_etter_kalAKG <- 0
-for (i in which(dat$d_selskap_tilkal_dummy == 1)){
-        (dat$d_kostnadsnorm_kalAKG/dat$d_kostnadsgrlag) 
-}
+dat$d_escore_etter_kalAKG <- (d_tilDEA$d_kostnadsnorm_kalAKG/dat$d_kostnadsgrlag) 
+
 
 
 
