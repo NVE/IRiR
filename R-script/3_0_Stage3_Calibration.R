551084a4a966fa7004a2630a75c90363c02de2d9
Skjaerven <ens@nve.no>
2017-01-09 16:21:06 +0100

    trinn 3 ok

--- R-script/3_0_Stage3_Calibration.R
+++ R-script/3_0_Stage3_Calibration.R
@@ -8,3 +8,8 @@ d_tilDEA$d_kostnadsgrlag <- ((d_tilDEA$fp_d_DV*faktisk.aar.kpiafaktor) + (d_tilD
 
 
 d_calib=NVE_cal(eff=d_tilDEA$d_deares_til_kal, cost_base=d_tilDEA$d_kostnadsgrlag, RAB=d_tilDEA$d_akg)
+
+r_tilDEA$r_kostnadsgrlag <- ((r_tilDEA$fp_r_DV*faktisk.aar.kpiafaktor) + (r_tilDEA$r_akg*nve.rente.t) + 
+                                     r_tilDEA$r_avs + (r_tilDEA$r_kile*faktisk.aar.kpifaktor))
+
+r_calib=NVE_cal(eff=r_tilDEA$r_deares_tilkal, cost_base = r_tilDEA$r_kostnadsgrlag, RAB = r_tilDEA$r_akg)
